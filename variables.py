794a0f57c966f4a56a4d2579cdcbb0ed517c26bd
alpha-martinez <martinezalphaa@gmail.com>
2020-09-21 17:25:25 -0400

    adding python lesson and pete's code to sort

---
+++ variables.py
@@ -0,0 +1,10 @@
+# Create variables:
+
+my_age = 24
+half_my_age = 24 / 2
+greeting = "Welcome to my github!"
+name = "Alpha"
+greeting_with_name = f'Welcome, {name} to my github!'
+# Assign values to each using your knowledge of division and concatenation!
+print(greeting_with_name)
+
